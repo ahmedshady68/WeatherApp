87801fc01180c7ef87df9594042d14a172497ea2
shady <ahmedshady68@gmail.com>
2023-04-15 04:43:57 +0200

    handle search keyboard button

--- app/src/main/java/com/shady/myweatherapp/ui/main/composable/search/SearchView.kt
+++ app/src/main/java/com/shady/myweatherapp/ui/main/composable/search/SearchView.kt
@@ -7,15 +7,17 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.rounded.KeyboardArrowDown
+import androidx.compose.material.icons.rounded.KeyboardArrowUp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
@@ -39,7 +41,8 @@ fun SearchView(
         searchListResult = searchListResult,
         onClickSearch = onClickSearch,
         onClickCity = onClickCity,
-        onSearchViewVisibilityChange = onSearchViewVisibilityChange)
+        onSearchViewVisibilityChange = onSearchViewVisibilityChange
+    )
 }
 
 @Composable
@@ -59,119 +62,137 @@ fun SearchContent(
     var cardRounded by remember {
         mutableStateOf(RoundedCornerShape(bottomEnd = 25.dp, bottomStart = 25.dp))
     }
-    if (isSearchViewDisplayed){
-    Column(
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        Card(
-            modifier = Modifier.fillMaxWidth(), shape = cardRounded
+    val focusManager = LocalFocusManager.current
+    if (isSearchViewDisplayed) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 26.dp, end = 32.dp, top = 27.dp, bottom = 27.dp)
-                    .background(Color.White)
-            ) {
-                IconButton(onClick = {
-                    if (searchText.isNotEmpty()) {
-                        searchButtonClick = searchText.isNotEmpty()
-                        cardRounded = RoundedCornerShape(bottomEnd = 0.dp, bottomStart = 0.dp)
-                        onClickSearch(searchText)
-                    }
-                }) {
-                    Icon(
-                        modifier = Modifier.size(24.dp),
-                        painter = painterResource(id = R.drawable.arrow_go_search_button),
-                        contentDescription = "close",
-                        tint = Color.Black
-                    )
-                }
-                TextField(modifier = Modifier
-                    .fillMaxWidth()
-                    .height(55.dp)
-                    .border(
-                        width = 1.dp, color = Color.Black, shape = RoundedCornerShape(15.dp)
-                    ),
-                    value = searchText,
-                    onValueChange = {
-                        searchText = it
-                    },
-                    colors = TextFieldDefaults.textFieldColors(
-                        backgroundColor = Color.White,
-                        focusedIndicatorColor = Color.White,
-                        unfocusedIndicatorColor = Color.White,
-                        cursorColor = Color.Black
-                    ),
-                    textStyle = TextStyle(
-                        color = Color.Black, fontSize = 16.sp
-                    ),
-                    label = { Text(text = "Search City", color = Color.Black) },
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Text, imeAction = ImeAction.Search
-                    ),
-                    singleLine = true,
-                    trailingIcon = {
-                        if (searchText.isNotEmpty()) {
-                            IconButton(onClick = {
-                                searchButtonClick = false
-                                cardRounded =
-                                    RoundedCornerShape(bottomEnd = 25.dp, bottomStart = 25.dp)
-                                searchText = ""
-                            }) {
-                                Icon(
-                                    modifier = Modifier.size(24.dp),
-                                    imageVector = Icons.Filled.Close,
-                                    contentDescription = "close",
-                                    tint = Color.Black
-                                )
-                            }
-                        }
-                    })
-            }
-        }
-        AnimatedVisibility(visible = searchButtonClick) {
             Card(
-                modifier = Modifier.fillMaxWidth(),
-                shape = RoundedCornerShape(bottomEnd = 25.dp, bottomStart = 25.dp)
+                modifier = Modifier.fillMaxWidth(), shape = cardRounded
             ) {
-                Column {
-                    LazyColumn(
-                        modifier = Modifier.heightIn(max = 150.dp),
-                    ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 26.dp, end = 32.dp, top = 27.dp, bottom = 27.dp)
+                        .background(Color.White)
+                ) {
+                    IconButton(onClick = {
                         if (searchText.isNotEmpty()) {
-                            if (searchListResult != null) {
-                                items(
-                                    searchListResult.toList()
-                                ) {
-                                    CategoryItems(title = it.name, subTitle = it.region) { title ->
-                                        searchText = title
-                                        searchButtonClick = false
-                                        onSearchViewVisibilityChange.invoke()
-                                        cardRounded =
-                                            RoundedCornerShape(bottomEnd = 25.dp, bottomStart = 25.dp)
-                                        onClickCity(title)
+                            searchButtonClick = searchText.isNotEmpty()
+                            cardRounded = RoundedCornerShape(bottomEnd = 0.dp, bottomStart = 0.dp)
+                            onClickSearch(searchText)
+                        }
+                    }) {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            painter = painterResource(id = R.drawable.arrow_go_search_button),
+                            contentDescription = "close",
+                            tint = Color.Black
+                        )
+                    }
+                    TextField(modifier = Modifier
+                        .fillMaxWidth()
+                        .height(55.dp)
+                        .border(
+                            width = 1.dp, color = Color.Black, shape = RoundedCornerShape(15.dp)
+                        ),
+                        value = searchText,
+                        onValueChange = {
+                            searchText = it
+                        },
+                        colors = TextFieldDefaults.textFieldColors(
+                            backgroundColor = Color.White,
+                            focusedIndicatorColor = Color.White,
+                            unfocusedIndicatorColor = Color.White,
+                            cursorColor = Color.Black
+                        ),
+                        textStyle = TextStyle(
+                            color = Color.Black, fontSize = 16.sp
+                        ),
+                        label = { Text(text = "Search City", color = Color.Black) },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Text, imeAction = ImeAction.Search
+                        ), keyboardActions = KeyboardActions(
+                            onSearch = {
+                                focusManager.clearFocus()
+                                if (searchText.isNotEmpty()) {
+                                    searchButtonClick = searchText.isNotEmpty()
+                                    cardRounded =
+                                        RoundedCornerShape(bottomEnd = 0.dp, bottomStart = 0.dp)
+                                    onClickSearch(searchText)
+                                }
+                            }),
+                        singleLine = true,
+                        trailingIcon = {
+                            if (searchText.isNotEmpty()) {
+                                IconButton(onClick = {
+                                    searchButtonClick = false
+                                    cardRounded =
+                                        RoundedCornerShape(bottomEnd = 25.dp, bottomStart = 25.dp)
+                                    searchText = ""
+                                }) {
+                                    Icon(
+                                        modifier = Modifier.size(24.dp),
+                                        imageVector = Icons.Filled.Close,
+                                        contentDescription = "close",
+                                        tint = Color.Black
+                                    )
+                                }
+                            }
+                        })
+                }
+            }
+            AnimatedVisibility(visible = searchButtonClick) {
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(bottomEnd = 25.dp, bottomStart = 25.dp)
+                ) {
+                    Column {
+                        LazyColumn(
+                            modifier = Modifier.heightIn(max = 150.dp),
+                        ) {
+                            if (searchText.isNotEmpty()) {
+                                if (searchListResult != null) {
+                                    items(
+                                        searchListResult.toList()
+                                    ) {
+                                        CategoryItems(
+                                            title = it.name,
+                                            subTitle = it.region
+                                        ) { title ->
+                                            searchText = title
+                                            searchButtonClick = false
+                                            onSearchViewVisibilityChange.invoke()
+                                            cardRounded =
+                                                RoundedCornerShape(
+                                                    bottomEnd = 25.dp,
+                                                    bottomStart = 25.dp
+                                                )
+                                            onClickCity(title)
+                                        }
                                     }
                                 }
                             }
                         }
-                    }
-                    IconButton(
-                        onClick = {
-                            searchButtonClick = false
-                            cardRounded = RoundedCornerShape(bottomEnd = 25.dp, bottomStart = 25.dp)
-                        }, modifier = Modifier
-                            .fillMaxWidth()
-                            .align(Alignment.CenterHorizontally)
-                    ) {
-                        Icon(
-                            modifier = Modifier.size(24.dp),
-                            imageVector = Icons.Rounded.KeyboardArrowDown,
-                            contentDescription = "arrow",
-                            tint = Color.Black
-                        )
+                        IconButton(
+                            onClick = {
+                                searchButtonClick = false
+                                cardRounded =
+                                    RoundedCornerShape(bottomEnd = 25.dp, bottomStart = 25.dp)
+                            }, modifier = Modifier
+                                .fillMaxWidth()
+                                .align(Alignment.CenterHorizontally)
+                        ) {
+                            Icon(
+                                modifier = Modifier.size(24.dp),
+                                imageVector = Icons.Rounded.KeyboardArrowUp,
+                                contentDescription = "arrow",
+                                tint = Color.Black
+                            )
+                        }
                     }
                 }
             }
         }
     }
-}}
+}
