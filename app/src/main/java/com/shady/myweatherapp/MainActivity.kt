b5eae5989ddd839e20f026bff2d3686b35436ede
shady <ahmedshady68@gmail.com>
2023-04-15 03:08:09 +0200

    test git

--- app/src/main/java/com/shady/myweatherapp/MainActivity.kt
+++ app/src/main/java/com/shady/myweatherapp/MainActivity.kt
@@ -46,7 +46,7 @@ class MainActivity : ComponentActivity() {
                     }
                     Greeting(temp)
                 }*/
-                Greeting(temp)
+                Greeting("test")
             }
         }
     }
